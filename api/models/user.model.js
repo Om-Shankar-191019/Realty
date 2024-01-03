22b049a87e4aa79e2d35716669322ec1b00d9e04
om shankar <om8003golu@gmail.com>
2024-01-03 12:07:50 +0530

    update header and make profile page private

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -16,6 +16,11 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    avatar: {
+      type: String,
+      default:
+        "https://cdn.pixabay.com/photo/2012/04/26/19/43/profile-42914_1280.png",
+    },
   },
   { timestamps: true }
 );
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { FaSearch } from "react-icons/fa";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 const Header = () => {
+  const currentUser = useSelector((state) => state.user.currentUser);
+
   return (
     <header className="bg-slate-200 shadow-lg">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -34,9 +37,16 @@ const Header = () => {
               About
             </li>
           </Link>
-          <Link to="/sign-in">
-            {" "}
-            <li className=" text-slate-700 hover:underline">Sign in</li>
+          <Link to="/profile">
+            {currentUser ? (
+              <img
+                src={currentUser.avatar}
+                alt="profile"
+                className="rounded-full h-7 w-7 object-cover"
+              />
+            ) : (
+              <li className=" text-slate-700 hover:underline">Sign in</li>
+            )}
           </Link>
         </ul>
       </div>
