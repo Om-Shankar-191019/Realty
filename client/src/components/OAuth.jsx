eaa5909429d4c16159a7a1ef4a5a2832b2c3b453
om shankar <om8003golu@gmail.com>
2024-01-04 08:34:52 +0530

    create profile page ui

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -38,7 +38,7 @@ const OAuth = () => {
       onClick={handleGoogleClick}
       className="bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95"
     >
-      OAuth
+      continue with google
     </button>
   );
 };
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const { currentUser } = useSelector((state) => state.user);
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+      <form className="flex flex-col gap-4">
+        <img
+          src={currentUser.avatar}
+          alt="profile"
+          className="rounded-full h-24 w-24 object-cover cursor-pointer mt-2 self-center"
+        />
+        <input
+          type="text"
+          placeholder="username"
+          id="username"
+          className="border p-3 rounded-lg"
+        />
+        <input
+          type="text"
+          placeholder="password"
+          id="password"
+          className="border p-3 rounded-lg"
+          // onChange={handleChange}
+        />
+
+        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+          update
+        </button>
+      </form>
+      <div className="flex justify-between mt-5">
+        <span className="text-red-700 cursor-pointer ">Delete account</span>
+        <span className="text-red-700 cursor-pointer ">Sign Out</span>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
