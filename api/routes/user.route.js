96248b384367464fa26671e60f429fedc109ac91
om shankar <om8003golu@gmail.com>
2024-01-07 21:42:17 +0530

    Add contact landlord functionality to the listing page

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   deleteUser,
+  getUser,
   getUserListings,
   test,
   updateUser,
@@ -12,5 +13,6 @@ router.get("/test", test);
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
 router.get("/listings/:id", verifyToken, getUserListings);
+router.get("/:id", verifyToken, getUser);
 
 export default router;
--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -14,6 +14,7 @@ import {
   FaParking,
   FaShare,
 } from "react-icons/fa";
+import Contact from "../components/Contact";
 // import Contact from '../components/Contact';
 
 const Listing = () => {
@@ -134,15 +135,15 @@ const Listing = () => {
                 {listing.furnished ? "Furnished" : "Unfurnished"}
               </li>
             </ul>
-            {/* {currentUser && listing.userRef !== currentUser._id && !contact && (
+            {currentUser && listing.userRef !== currentUser._id && !contact && (
               <button
                 onClick={() => setContact(true)}
                 className="bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3"
               >
                 Contact landlord
               </button>
-            )} */}
-            {/* {contact && <Contact listing={listing} />} */}
+            )}
+            {contact && <Contact listing={listing} />}
           </div>
         </div>
       )}
