23c1f8c75ce995bdea3ed5a98e561bc6e18e14f2
om shankar <om8003golu@gmail.com>
2024-01-08 21:23:17 +0530

    header search form functionality (input and url search term interactive)

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -3,6 +3,7 @@ import {
   createListing,
   deleteListing,
   getListing,
+  getListings,
   updateListing,
 } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
@@ -12,5 +13,6 @@ router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
 router.post("/update/:id", verifyToken, updateListing);
 router.get("/get/:id", getListing);
+router.get("/get", getListings);
 
 export default router;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const Home = () => {
+  useEffect(() => {
+    const fetchListing = async () => {
+      try {
+        // setLoading(true);
+        const res = await fetch(`/api/listing/get`);
+        const data = await res.json();
+        // if (data.success === false) {
+        //   setError(true);
+        //   setLoading(false);
+        //   return;
+        // }
+        // setListing(data);
+        // setLoading(false);
+        // setError(false);
+        // console.log(data);
+      } catch (error) {
+        // setError(true);
+        // setLoading(false);
+        console.log(error);
+      }
+    };
+
+    fetchListing();
+  }, []);
   return <div>Home</div>;
 };
 
