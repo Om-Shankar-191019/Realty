6b747d681351b880613a6f91f098b0b6115cf88d
om shankar <om8003golu@gmail.com>
2024-01-15 10:52:10 +0530

    variable port

--- api/index.js
+++ api/index.js
@@ -20,7 +20,7 @@ mongoose
   .then(() => console.log("connected to mongoDB"))
   .catch((err) => console.log(err));
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`server is running on port ${port}...`);
 });
